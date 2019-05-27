55b7ac79f457d4d899c1e9c3b1ed5f6c7a586cc9
Leandro A. Siqueira <vrx_lasiqueira@uolinc.com>
2019-05-27 19:08:15 -0300

    C1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+import { StoreProvider } from "./Store";
+
+ReactDom.render(
+  <StoreProvider>
+    <App />
+  </StoreProvider>,
+  document.getElementById("root")
+);
